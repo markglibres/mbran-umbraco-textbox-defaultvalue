b9eb30cf0d5467834b140e971600febb801481de
Mark Gil Libres <markglibres@gmail.com>
2018-04-27 16:53:02 +1000

    initial commit

---
+++ src/MBran.Textbox.DefaultValue/App_Plugins/MBran.Textbox.DefaultValue/textbox.DefaultValue.Controller.js
@@ -0,0 +1,34 @@
+﻿angular.module("umbraco").controller("MBran.Textbox.DefaultValueController", [
+    "$scope",
+    function ($scope) {
+
+        initModels();
+
+        if ($scope.model.config.showIfBlank) {
+
+            setDefaultValue();
+            $scope.$watch('scope.textValue', function (newValue, oldValue) {
+                if (!newValue) setDefaultValue();
+            });
+
+        }
+
+        function setDefaultValue() {
+            if (!$scope.model.value.default) {
+                angular.copy($scope.model.value.default, $scope.textValue);    
+            }
+        }
+
+        function initModels() {
+            if (!$scope.model.value || $scope.model.value==="") {
+                $scope.model.value = {
+                    default: "",
+                    text: ""
+                };
+            }
+            $scope.model.value.default = $scope.model.config.defaultValue;
+
+        }
+
+    }
+]);
---
+++ src/MBran.Textbox.DefaultValue/TextboxDefaultValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace MBran.Textbox.DefaultValue
+{
+    [PropertyValueType(typeof(string))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class TextboxDefaultValueConverter : IPropertyValueConverter
+    {
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return JsonConvert.DeserializeObject<TextboxDefaultValue>(source as string);
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var data = source as TextboxDefaultValue;
+            return string.IsNullOrWhiteSpace(data?.Value) ? data?.Default : data.Value;
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("MBran.Textbox.DefaultValue",
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
